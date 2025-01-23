ba89440ad60ca3a7d70e59616e12d7c31b1371a1
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-01-23 17:46:42 +0000

    Visual improvements, creating a contact fully works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import ContactList from "./ContactList";
+import ContactForm from "./ContactForm";
 import "./App.css";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     fetchContacts();
@@ -13,10 +15,34 @@ function App() {
     const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
-    console.log(data.contacts);
   };
 
-  return <ContactList contacts={contacts} />;
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) {
+      setIsModalOpen(true);
+    }
+  };
+
+  return (
+    <>
+      <ContactList contacts={contacts} />
+      <button onClick={openCreateModal}>Create Contact</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close" onClick={closeModal}>
+              &times;
+            </button>
+            <ContactForm />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
